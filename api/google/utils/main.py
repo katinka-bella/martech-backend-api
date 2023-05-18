c9585cbf0a5bbcf1f167335c913c181ae33ca371
katinka-bella <kdovzhanytsia@gmail.com>
2023-05-18 13:20:32 +0200

    update google utils operator

--- api/google/utils/main.py
+++ api/google/utils/main.py
@@ -7,29 +7,30 @@ SCOPES = [
 
 REDIRECT_URL = "https://localhost:8080/callback"
 
-def get_access_token(
-    CLIENT_ID, CLIENT_SECRET, SCOPES, PATH_SECRETS_FILE, REDIRECT_URL
-    ):
-    from google.oauth2.credentials import Credentials
-    from google_auth_oauthlib.flow import InstalledAppFlow
+class utils_google:
+    def create_access_token(
+        CLIENT_ID, CLIENT_SECRET, SCOPES, PATH_SECRETS_FILE, REDIRECT_URL
+        ):
+        from google.oauth2.credentials import Credentials
+        from google_auth_oauthlib.flow import InstalledAppFlow
 
-    # create the flow object
-    flow = InstalledAppFlow.from_client_secrets_file(
-        PATH_SECRETS_FILE,
-        scopes=SCOPES,
-        redirect_uri=REDIRECT_URL)
+        # create the flow object
+        flow = InstalledAppFlow.from_client_secrets_file(
+            PATH_SECRETS_FILE,
+            scopes=SCOPES,
+            redirect_uri=REDIRECT_URL)
 
-    # get authorization URL
-    auth_url, _ = flow.authorization_url(access_type='offline')
+        # get authorization URL
+        auth_url, _ = flow.authorization_url(access_type='offline')
 
-    # open authorization URL in a web browser and authorize the application
-    print("Please go to this URL: {}".format(auth_url))
-    authorization_response = input("Enter the full callback URL: ")
+        # open authorization URL in a web browser and authorize the application
+        print("Please go to this URL: {}".format(auth_url))
+        authorization_response = input("Enter the full callback URL: ")
 
-    # get the authorization code from the response
-    flow.fetch_token(authorization_response=authorization_response)
+        # get the authorization code from the response
+        flow.fetch_token(authorization_response=authorization_response)
 
-    # create credentials object from the flow object
-    creds = flow.credentials
+        # create credentials object from the flow object
+        creds = flow.credentials
 
-    return creds.token
+        return creds.token
